c3989f85c4e7954ab57c2cb064d0e9e9e8795f3c
Casey Chu <casey@bitsofpancake.com>
2017-03-08 19:08:41 -0800

    small dimension error

--- news-funnel/model.py
+++ news-funnel/model.py
@@ -134,7 +134,7 @@ class RushModel:
             for i in range(self.config.summary_length):
                 context = tf.slice(padded_predictions, [0, i], [-1, self.config.context_size])
                 logits = self.do_prediction_step(articles, context)
-                padded_predictions = tf.concat_v2([padded_predictions, tf.to_int32(tf.argmax(logits, axis=1))], 1)
+                padded_predictions = tf.concat_v2([padded_predictions, tf.expand_dims(tf.to_int32(tf.argmax(logits, axis=1)), -1)], 1)
             return tf.slice(padded_predictions, [0, self.config.context_size], [-1, -1])
         """
         elif method == "beam":
