2defac727301fc028b52dd0c3bb8131a6659e428
Casey Chu <casey@bitsofpancake.com>
2017-03-08 19:07:02 -0800

    More beam search

--- news-funnel/model.py
+++ news-funnel/model.py
@@ -146,9 +146,21 @@ class RushModel:
                 log_probs = prediction_log_probs + tf.nn.log_softmax(logits=do_prediction_step(articles, context)) 
                 assert log_probs.get_shape() == (self.config.batch_size, self.config.beam_size, self.config.vocab_size)
             
-                best_log_probs, best_indices = tf.nn.top_k(input=log_probs, k=self.config.beam_size) 
+                best_log_probs, best_words = tf.nn.top_k(input=log_probs, k=self.config.beam_size) 
+                assert best_log_probs.get_shape() == (self.config.batch_size, self.config.beam_size, self.config.beam_size)
+                assert best_words.get_shape() == (self.config.batch_size, self.config.beam_size, self.config.beam_size)
                 
-                tf.reshape(best_log_probs, (self.batch_size, self.beam_search**2))
+                best_log_probs = tf.reshape(best_log_probs, (self.config.batch_size, self.config.beam_size**2))
+                best_words = tf.reshape(best_words, (self.config.batch_size, self.config.beam_size**2))
+                prediction_log_probs, best_indices = tf.nn.top_k(input=best_log_probs, k=self.config.beam_size) 
+                assert prediction_log_probs.get_shape() == (self.config.batch_size, self.config.beam_size)
+                assert best_indices.get_shape() == (self.config.batch_size, self.config.beam_size)
+                
+                best_beams = tf.mod(best_indices, self.config.beam_size)
+                best_subbeams = tf.truncatediv(best_indices, self.config.beam_size)
+                
+                
+                
                 # dimensions: batch, beam, beam
                 
                 
