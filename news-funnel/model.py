e4703d15553eb76f2975166f7940882d21a8d4df
Casey Chu <casey@bitsofpancake.com>
2017-03-08 23:23:59 -0800

    axis 1

--- news-funnel/model.py
+++ news-funnel/model.py
@@ -125,7 +125,7 @@ class RushModel:
                     tf.tile(tf.expand_dims(tf.expand_dims(start_embedding, 0), 0), [self.config.batch_size, self.config.smoothing_window, 1]),
                     embedded_input,
                     tf.tile(tf.expand_dims(tf.expand_dims(end_embedding, 0), 0), [self.config.batch_size, self.config.smoothing_window, 1])
-                ], 2)
+                ], 1)
                 smoothed_input = tf.zeros_like(input_embeddings)
                 for i in xrange(self.config.smoothing_window):
                     smoothed_input += tf.slice(padded_input, [0, i, 0], [-1, self.config.smoothing_window, -1])
