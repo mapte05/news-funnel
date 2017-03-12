04776d2b9d292c8ecaee9b972cd81276887d3ad4
liam <liamk@stanford.edu>
2017-03-12 05:16:51 -0700

    gradient clipping

--- news-funnel/model.py
+++ news-funnel/model.py
@@ -247,12 +247,13 @@ class RushModel:
         learning_rate = tf.train.exponential_decay(self.config.lr, global_step, self.config.lr_decay_after_steps, self.config.lr_decay_base, staircase=self.config.lr_staircase)
         optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 
-        training_op = optimizer.minimize(loss, global_step=global_step)
+        #train_op = optimizer.minimize(loss, global_step=global_step)
         grads, vars = zip(*optimizer.compute_gradients(loss))
 
         grads, _ = tf.clip_by_global_norm(grads, self.config.max_grad_norm)
+        train_op = optimizer.apply_gradients(zip(grads, vars), global_step=global_step)
 
-        return training_op, tf.global_norm(grads), learning_rate
+        return train_op, tf.global_norm(grads), learning_rate
 
 def write_config(config, config_file):
     with open(config_file, 'wb') as outf:
