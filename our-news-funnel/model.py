ed63cc1fd54ac944ec6e40906b4a701fbc76a15a
Casey Chu <casey@bitsofpancake.com>
2017-03-13 19:06:02 -0700

    small test interval

--- our-news-funnel/model.py
+++ our-news-funnel/model.py
@@ -43,7 +43,7 @@ class Config(object):
     beam_size = 5
     encoding_method = "attention" # "attention" or "bag-of-words"
     
-    test_interval = 2500
+    test_interval = 50
     renormalize_interval = 10000
     
     max_vocab = 75000 # Nallapati 150k
