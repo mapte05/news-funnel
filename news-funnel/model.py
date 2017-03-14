564d0e2c7a0e2a8040bf974dd91ef16705211ed8
Casey Chu <casey@bitsofpancake.com>
2017-03-13 17:33:16 -0700

    normalize first

--- news-funnel/model.py
+++ news-funnel/model.py
@@ -85,7 +85,7 @@ class Config(object):
 class RushModel:
 
     def __init__(self, word2vec_embeddings, config):
-        self.word2vec_embeddings = tf.nn.l2_normalize(word2vec_embeddings)
+        self.word2vec_embeddings = tf.nn.l2_normalize(word2vec_embeddings, 2)
         self.config = config
         self.defined = False
 
