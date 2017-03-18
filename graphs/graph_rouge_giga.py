31f66f01149f0fa9e668b06f371f4045ad999509
Maneesh Apte <maneesh@kanjoya.com>
2017-03-17 19:26:03 -0700

    csv data for graphs

---
+++ graphs/graph_rouge_giga.py
@@ -0,0 +1,17 @@
+
+"""
+Eval_overfit is rush baseline summaries without regularization
+~/news-funnel/results/eval_overfit
+Eval_sample2 is  rush + sample softmax summaries with regularization
+~/news-funnel/results/eval_sampled2
+"""
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
